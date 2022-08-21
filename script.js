91c6d7fd31fae979f6059f9a92b035effccc9b6e
Николай Васильев <nikolay.vasilyev@uksever-invest.ru>
2022-08-21 12:15:09 +0300

    add flexes

--- script.js
+++ script.js
@@ -4,30 +4,30 @@ let callMeButton = document.getElementById("callMeButton")
 let slideLeft = document.getElementById("slideLeft")
 let slideRight = document.getElementById("slideRight")
 let modal = document.getElementsByClassName("modal")[0]
-let slider = document.getElementsByClassName("slider")[0]
-
+let slider = document.getElementsByClassName("slider-container__slider")[0]
+let num_sliders = slider.children.length
 let sliderDistance = 0
 
 slideLeft.onclick = function () {
-    console.log("слайд на лево")
-    sliderDistance -= 50;
+    console.log("слайд налево")
+    sliderDistance -= 525;
     console.log(sliderDistance)
 
     if (sliderDistance < 0) {
         console.log("слайдер начало в конец");
-        sliderDistance = slider.children.length * 50 - 100;
+        sliderDistance = slider.children.length * 525 - 1050;
     }
-    slider.style.transform = `translateX(-${sliderDistance}%)`
+    slider.style.transform = `translateX(-${sliderDistance}px)`
 }
 
 slideRight.onclick = function () {
-    console.log("слайд на право");
-    sliderDistance += 50;
+    console.log("слайд направо");
+    sliderDistance += 525;
     console.log(sliderDistance);
 
-    if (sliderDistance == (slider.children.length * 50 - 50)) {
+    if (sliderDistance > (slider.children.length * 525 - 1050)) {
         console.log("слайдер конец в начало");
         sliderDistance = 0;
     }
-    slider.style.transform = `translateX(-${sliderDistance}%)`
+    slider.style.transform = `translateX(-${sliderDistance}px)`
 }
